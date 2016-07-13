40f56e8a2e8d48761cab858cf081bfd25ef2616c
magnusfriis <mf@abtion.com>
2016-07-13 16:24:36 +0200

    animation time

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ jQuery(function($) {
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+500) {
+			if (imagePos < topOfWindow+100) {
 				$(this).addClass("slideRight");
 			}
 		});
